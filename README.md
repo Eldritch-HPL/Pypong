0a81655711e215d50e795deda8962931d8e5a832
cactusrich <39964782+Eldritch-HPL@users.noreply.github.com>
2021-09-26 22:35:33 -0500

    Initial File Load

---
+++ README.md
@@ -0,0 +1,4 @@
+An old skool Pong game with old skool coding. Based on a freeCodeCamp tutorial
+with added sounds, titles, and improved paddle function (they no longer go offscreen)
+as well as ending the game before it gets boring. A good workout using the turtle
+module rather than pygame. No specific requirements.
---
+++ pypong.py
@@ -0,0 +1,153 @@
+"""
+An old skool Pong game with old skool coding. Based on a freeCodeCamp tutorial
+with added sounds, titles, and improved paddle function (they no longer go offscreen)
+as well as ending the game before it gets boring. A good workout using the turtle
+module rather than pygame. No specific requirements.
+"""
+
+import turtle
+import winsound
+
+# Paddle Movement Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Initialize Score
+score_a = 0
+score_b = 0
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+
+# Paddle A
+paddle_a=turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b=turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball=turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.15
+ball.dy = 0.15
+
+# Start screen with title and sound, then score screen
+wn = turtle.Screen()
+wn.title("Pong by cactusrich")
+wn.bgcolor("black")
+wn.setup(width=800, height = 600)
+wn.tracer(0)
+pen.goto(0, 100)
+pen.write("PONG!", align="center", font=("Courier", 50, "bold"))
+winsound.PlaySound("start.wav", winsound.SND_FILENAME)
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("pong.wav", winsound.SND_ASYNC)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("pong.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    # stop paddles at border
+    if paddle_a.ycor() > 250:
+        paddle_a.sety(250)
+
+    if paddle_a.ycor() < -250:
+        paddle_a.sety(-250)
+
+    if paddle_b.ycor() > 250:
+        paddle_b.sety(250)
+
+    if paddle_b.ycor() < -250:
+        paddle_b.sety(-250)
+
+
+    # Paddle and Ball Collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *=-1
+        winsound.PlaySound("pong.wav", winsound.SND_ASYNC)
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *=-1 
+        winsound.PlaySound("pong.wav", winsound.SND_ASYNC)
+
+    # End game at 12
+    if (score_a or score_b) >= 12:
+        pen.goto(0, 100)
+        pen.write("Game Over!", align="center", font=("Courier", 50, "bold"))
+        winsound.PlaySound("end.wav", winsound.SND_FILENAME)
+        break
